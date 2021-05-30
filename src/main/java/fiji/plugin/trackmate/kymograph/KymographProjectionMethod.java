f9e0f8a2410bd17b7797fa9e2933a71c3c4a4827
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2021-05-30 16:37:28 +0200

    KymographProjectionMethod can generate an accumulator its two methods.

--- src/main/java/fiji/plugin/trackmate/kymograph/KymographProjectionMethod.java
+++ src/main/java/fiji/plugin/trackmate/kymograph/KymographProjectionMethod.java
@@ -1,5 +1,7 @@
 package fiji.plugin.trackmate.kymograph;
 
+import java.util.Arrays;
+
 public enum KymographProjectionMethod
 {
 
@@ -17,5 +19,85 @@ public enum KymographProjectionMethod
 	public String toString()
 	{
 		return name;
+	}
+
+	public Accumulator accumulator()
+	{
+		switch ( this )
+		{
+		case MEAN:
+			return new MeanAccumulator();
+		case MIP:
+		default:
+			return new MaxAccumulator();
+		}
+	}
+
+	public static interface Accumulator
+	{
+
+		public void accumulate( double[] line );
+
+		public double[] get();
+	}
+
+	private static class MeanAccumulator implements Accumulator
+	{
+
+		private double[] sum;
+
+		private int n = 0;
+
+		@Override
+		public void accumulate( final double[] line )
+		{
+			if ( sum == null )
+			{
+				sum = Arrays.copyOf( line, line.length );
+				n = 1;
+				return;
+			}
+
+			n++;
+			for ( int i = 0; i < Math.max( line.length, sum.length ); i++ )
+				sum[ i ] += line[ i ];
+		}
+
+		@Override
+		public double[] get()
+		{
+			if ( sum == null )
+				return null;
+
+			for ( int i = 0; i < sum.length; i++ )
+				sum[ i ] /= n;
+
+			return sum;
+		}
+	}
+
+	private static class MaxAccumulator implements Accumulator
+	{
+
+		private double[] storage;
+
+		@Override
+		public void accumulate( final double[] line )
+		{
+			if ( storage == null )
+			{
+				storage = Arrays.copyOf( line, line.length );
+				return;
+			}
+
+			for ( int i = 0; i < Math.max( line.length, storage.length ); i++ )
+				storage[ i ] = Math.max( storage[ i ], line[ i ] );
+		}
+
+		@Override
+		public double[] get()
+		{
+			return storage;
+		}
 	}
 }
