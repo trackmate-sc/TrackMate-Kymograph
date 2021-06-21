82eba333ccfb231e6f34c053f3ad512b97da5712
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2021-06-21 16:20:08 +0200

    Static method to reload a kymograph tracing session.
    From a JSon file and the source image.

--- src/main/java/fiji/plugin/trackmate/kymograph/tracing/ui/KymographTracingController.java
+++ src/main/java/fiji/plugin/trackmate/kymograph/tracing/ui/KymographTracingController.java
@@ -1,7 +1,15 @@
 package fiji.plugin.trackmate.kymograph.tracing.ui;
 
+import java.awt.Component;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import fiji.plugin.trackmate.gui.GuiUtils;
 import fiji.plugin.trackmate.gui.Icons;
@@ -9,6 +17,7 @@ import fiji.plugin.trackmate.kymograph.tracing.KymographOverlay;
 import fiji.plugin.trackmate.kymograph.tracing.KymographTracer;
 import fiji.plugin.trackmate.kymograph.tracing.KymographTracingTool;
 import fiji.plugin.trackmate.kymograph.tracing.Kymographs;
+import fiji.plugin.trackmate.kymograph.tracing.KymographsIO;
 import fiji.plugin.trackmate.kymograph.tracing.TracingParameters;
 import fiji.plugin.trackmate.util.TMUtils;
 import ij.ImagePlus;
@@ -21,9 +30,11 @@ import net.imglib2.type.numeric.real.DoubleType;
 public class KymographTracingController
 {
 
+	private static JFileChooser fileChooser = new JFileChooser();
+
 	public KymographTracingController( final ImagePlus imp )
 	{
-		this( imp, new Kymographs() );
+		this( imp, new Kymographs( imp.getTitle() ) );
 	}
 
 	public KymographTracingController( final ImagePlus imp, final Kymographs kymographs )
@@ -55,17 +66,32 @@ public class KymographTracingController
 
 		// Wire some listeners.
 		panel.btnPreview.addActionListener( e -> SwingUtilities.invokeLater( () -> preview( imp, tracingParameters.getSigma() ) ) );
-		panel.btnSave.addActionListener( e -> save() );
-		panel.btnLoad.addActionListener( e -> load() );
+		panel.btnSave.addActionListener( e -> save( proposeJsonFile( imp ), frame ) );
 	}
 
-	private void load()
+	public static void load( final File kymographFile, final ImagePlus imp )
 	{
-		// TODO Auto-generated method stub
-		System.out.println( "TODO load" ); // DEBUG
+		fileChooser.setFileFilter( new FileNameExtensionFilter( "JSon files:", ".json" ) );
+		fileChooser.setSelectedFile( kymographFile );
+		fileChooser.setDialogTitle( "Load from a JSon file" );
+		final int returnVal = fileChooser.showOpenDialog( null );
+		if ( returnVal == JFileChooser.APPROVE_OPTION )
+		{
+			final File selectedFile = fileChooser.getSelectedFile();
+			try
+			{
+				final String str = new String( Files.readAllBytes( selectedFile.toPath() ) );
+				final Kymographs kymographs = KymographsIO.fromJson( str );
+				new KymographTracingController( imp, kymographs );
+			}
+			catch ( final IOException e )
+			{
+				e.printStackTrace();
+			}
+		}
 	}
 
-	private void save()
+	private void save( final File file, final Component parent )
 	{
 		// TODO Auto-generated method stub
 		System.out.println( "TODO save" ); // DEBUG
@@ -82,5 +108,32 @@ public class KymographTracingController
 		@SuppressWarnings( { "unchecked" } )
 		final Img< DoubleType > filtered = KymographTracer.filterSlice( img, channel, z, frame, sigma );
 		ImageJFunctions.show( filtered, "Filtered_" + imp.getShortTitle() + "_Sigma_" + sigma );
+	}
+
+	private static final File proposeJsonFile( final ImagePlus imp )
+	{
+		File folder;
+		final String fileName;
+		if ( null != imp.getOriginalFileInfo() && null != imp.getOriginalFileInfo().directory )
+		{
+			final String directory = imp.getOriginalFileInfo().directory;
+			folder = Paths.get( directory ).toAbsolutePath().toFile();
+
+			if ( null != imp.getOriginalFileInfo().fileName )
+			{
+				final int i = imp.getOriginalFileInfo().fileName.lastIndexOf( '.' );
+				fileName = imp.getOriginalFileInfo().fileName.substring( 0, i ) + ".json";
+			}
+			else
+			{
+				fileName = "Kymographs.json";
+			}
+		}
+		else
+		{
+			folder = new File( System.getProperty( "user.dir" ) );
+			fileName = "Kymographs.json";
+		}
+		return new File( folder, fileName );
 	}
 }
