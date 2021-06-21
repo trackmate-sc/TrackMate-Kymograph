48ddf9ef081c4a2bc2b80ea3e01c21032a92983b
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2021-06-21 18:13:10 +0200

    Remove useless stuff from the test drive.

--- src/test/java/fiji/plugin/trackmate/kymograph/tracing/KymographTracingTestDrive.java
+++ src/test/java/fiji/plugin/trackmate/kymograph/tracing/KymographTracingTestDrive.java
@@ -4,7 +4,6 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 import fiji.plugin.trackmate.kymograph.tracing.ui.KymographTracingController;
-import fiji.plugin.trackmate.util.TMUtils;
 import ij.IJ;
 import ij.ImageJ;
 import ij.ImagePlus;
@@ -16,7 +15,6 @@ public class KymographTracingTestDrive
 	{
 		UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
 		ImageJ.main( args );
-		TMUtils.getContext();
 
 		final String path = "samples/Kymograph.tif";
 		final ImagePlus imp = IJ.openImage( path );
